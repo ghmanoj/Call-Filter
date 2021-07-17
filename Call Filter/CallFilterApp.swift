8a0315be1b4da7f96155a94dda8ff62ced475d3e
Manoj <ermanojghimire@gmail.com>
2021-07-16 21:19:40 -0700

    Initial Commit

---
+++ Call Filter/CallFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Call_FilterApp.swift
+//  Call Filter
+//
+//  Created by Manoj Ghimire on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CallFilterApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
---
+++ Call Filter/ViewExt.swift
@@ -0,0 +1,16 @@
+//
+//  ViewExt.swift
+//  Call Filter
+//
+//  Created by Manoj Ghimire on 7/15/21.
+//
+
+import SwiftUI
+
+
+
+extension UIScreen {
+	static let screenWidth = UIScreen.main.bounds.size.width
+	static let screenHeight = UIScreen.main.bounds.size.height
+}
+
